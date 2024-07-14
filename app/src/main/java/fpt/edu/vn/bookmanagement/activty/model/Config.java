3e3857fd9005b855a0afd5570b074e031e12d13f
thdo141 <do34dh@gmail.com>
2024-07-15 03:50:13 +0700

    bo sung

---
+++ app/src/main/java/fpt/edu/vn/bookmanagement/activty/model/Config.java
@@ -0,0 +1,8 @@
+package fpt.edu.vn.bookmanagement.activty.model;
+
+public class Config {
+    public static final String EMAIL ="donxthe161569@fpt.edu.vn";
+    public static final String PASSWORD ="thanhdo1411";
+
+    //public static final String PASSWORD ="jtss wjvj xnrc vstj";
+}
